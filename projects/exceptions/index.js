ec973a337504cfd1b4351684157ded84986ab0a2
Mihaela Rusnac <rusnac_mihaela@mail.ru>
2023-09-21 17:54:31 +0300

    Задачи 2 недели 2 часть

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -20,7 +20,30 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true (потому что все элементы массива меньше 10)
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false (потому что как минимум первый элемент больше 10)
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  var x = 0;
+    var y = 0;
+
+    if (( array == 0 ) || (array.length <= 0)) {
+        throw new Error("empty array");
+    } else if (typeof fn != 'function') {
+        throw new Error("fn is not a function");
+    } else {
+        for (var i = 0; i < array.length; i++) {
+            var z = fn(array[i]);
+            if (z == false) {
+                y++;
+            } else if (z == true) {
+                x++;
+            }
+            if (array.length == x) {
+                return true;
+            } else if (y > 0) {
+                return false;
+            }
+        }
+    }
+}
 
 /*
  Задание 2:
@@ -42,7 +65,30 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true (потому что в массиве есть хотя бы один элемент больше 20)
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false (потому что в массиве нет ни одного элемента больше 20)
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  var x = 0;
+
+    if (( array == 0 ) || (array.length <= 0)) {
+        throw new Error("empty array");
+    } else if (typeof fn != 'function') {
+        throw new Error("fn is not a function");
+    } else {
+
+        for (var i = 0; i < array.length; i++) {
+            var z = fn(array[i]);
+            if (z == true) {
+                x++;
+            }
+        }
+
+        if (x < 1) {
+            return false;
+        } else (x >= 1)
+        {
+            return true;
+        }
+    }
+}
 
 /*
  Задание 3:
@@ -56,7 +102,21 @@ function isSomeTrue(array, fn) {}
    - fn не является функцией (с текстом "fn is not a function")
      для проверки на функцию вам может помочь оператор typeof
  */
-function returnBadArguments() {}
+function returnBadArguments() {
+  var x = new Array();
+    var z = '';
+    if (typeof fn != 'function') {
+        throw new Error("fn is not a function");
+    }
+    for (var i = 1; i < arguments.length; i++) {
+        try {
+            z = fn(arguments[i]);
+        } catch (e) {
+            x.push(arguments[i]);
+        }
+    }
+    return x;
+}
 
 /*
  Задание 4:
@@ -84,7 +144,51 @@ function returnBadArguments() {}
    console.log(calc.div(2, 2)); // выведет 2.5 (10 / 2 / 2)
    console.log(calc.div(2, 0)); // выбросит исключение, потому что один из аргументов равен 0
  */
-function calculator(number) {}
+function calculator(number=0) {
+  if (typeof number != 'number') {
+    throw new Error("number is not a number");
+}
+
+var ob = {
+    sum: function () {
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i] === 0) {
+                throw new Error("division by 0");
+            }
+            number += arguments[i];
+        }
+        return number;
+    },
+    dif: function () {
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i] === 0) {
+                throw new Error("division by 0");
+            }
+            number -= arguments[i];
+        }
+        return number;
+    },
+    div: function () {
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i] === 0) {
+                throw new Error("division by 0");
+            }
+            number /= arguments[i];
+        }
+        return number;
+    },
+    mul: function () {
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i] === 0) {
+                throw new Error("division by 0");
+            }
+            number *= arguments[i];
+        }
+        return number;
+    }
+}
+return ob;
+}
 
 /* При решении задач, постарайтесь использовать отладчик */
 
