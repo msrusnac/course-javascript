ee31519dc35e0962d2a490958ce5ebd95fe1f005
Mihaela Rusnac <rusnac_mihaela@mail.ru>
2023-09-25 20:03:20 +0300

    Отредактированное индивидуальное задание 2 недели

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,128 +1,23 @@
-var friends = [
-  {
-    "id": 0,
-    "avatar": "https://via.placeholder.com/100?text=avatar",
-    "firstName": "Adrian",
-    "lastName": "Norman"
+import photosDB from './photos.json';
+import friendsDB from './friends.json';
+
+export default {
+  getRandomElement(array) {
+    if (!array.lenght) {
+      return null;
+    }
+    const index = Math.round(Math.random() * (array.lenght -1));
+    return array[index];
   },
-  {
-    "id": 1,
-    "avatar": "https://via.placeholder.com/100?text=avatar",
-    "firstName": "Gail",
-    "lastName": "Norton"
+
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB.get(friend.id);
+    const photo =this.getRandomElement(photos);
+
+    return {friend, url: photo.url};
   },
-  {
-    "id": 2,
-    "avatar": "https://via.placeholder.com/100?text=avatar",
-    "firstName": "Molina",
-    "lastName": "Rodgers"
-  },
-  {
-    "id": 3,
-    "avatar": "https://via.placeholder.com/100?text=avatar",
-    "firstName": "Adams",
-    "lastName": "Parrish"
-  },
-  {
-    "id": 4,
-    "avatar": "https://via.placeholder.com/100?text=avatar",
-    "firstName": "Mercer",
-    "lastName": "Wiggins"
-  }
-];
-
-var photos = {
-  "0": [
-    {
-      "id": 10,
-      "url": "https://via.placeholder.com/360x680?text=photo 1 for Adrian Norman"
-    },
-    {
-      "id": 11,
-      "url": "https://via.placeholder.com/360x680?text=photo 2 for Adrian Norman"
-    },
-    {
-      "id": 12,
-      "url": "https://via.placeholder.com/360x680?text=photo 3 for Adrian Norman"
-    }
-  ],
-  "1": [
-    {
-      "id": 20,
-      "url": "https://via.placeholder.com/360x680?text=photo 1 for Gail Norton"
-    },
-    {
-      "id": 21,
-      "url": "https://via.placeholder.com/360x680?text=photo 2 for Gail Norton"
-    },
-    {
-      "id": 22,
-      "url": "https://via.placeholder.com/360x680?text=photo 3 for Gail Norton"
-    }
-  ],
-  "2": [
-    {
-      "id": 30,
-      "url": "https://via.placeholder.com/360x680?text=photo 1 for Molina Rodgers"
-    },
-    {
-      "id": 31,
-      "url": "https://via.placeholder.com/360x680?text=photo 2 for Molina Rodgers"
-    },
-    {
-      "id": 32,
-      "url": "https://via.placeholder.com/360x680?text=photo 3 for Molina Rodgers"
-    }
-  ],
-  "3": [
-    {
-      "id": 40,
-      "url": "https://via.placeholder.com/360x680?text=photo 1 for Adams Parrish"
-    },
-    {
-      "id": 41,
-      "url": "https://via.placeholder.com/360x680?text=photo 2 for Adams Parrish"
-    },
-    {
-      "id": 42,
-      "url": "https://via.placeholder.com/360x680?text=photo 3 for Adams Parrish"
-    }
-  ],
-  "4": [
-    {
-      "id": 50,
-      "url": "https://via.placeholder.com/360x680?text=photo 1 for Mercer Wiggins"
-    },
-    {
-      "id": 51,
-      "url": "https://via.placeholder.com/360x680?text=photo 2 for Mercer Wiggins"
-    },
-    {
-      "id": 52,
-      "url": "https://via.placeholder.com/360x680?text=photo 3 for Mercer Wiggins"
-    }
-  ]
-}
-
-
-function getRandomElement(arr) {
-   var rand = Math.floor(Math.random() * arr.length);
-   var value = arr.map(item => item.firstName)
-   var result = value[rand];
-   return result;
-
-  function getNextPhoto (result) {
-    var rand2 = Math.floor(Math.random() * photos.length);
-    var value2 = photos.map(item => item.url)
-    var result2 = value2[rand2];
-    return result2;
-  };
-
-  getNextPhoto();
-
-
-}
-
+};
 
 
 console.log(getRandomElement(friends));
