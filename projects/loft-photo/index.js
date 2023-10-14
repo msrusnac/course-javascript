5250757cfa12503a6e1a87915fee7e8df99cda01
Mihaela <rusnac_mihaela@mail.com>
2023-10-14 13:58:02 +0300

    Задание 4 недели

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,36 +1,10 @@
 import pages from './pages';
-import('./styles.css');
-
-const pageNames = ['login', 'main', 'profile'];
-
-document.addEventListener('click', () => {
-  document.addEventListener('click', () => {
-    const pageName = model.getRandomElement(pagesNames);
-    pages.openPage(pageName);
-  });
-  
-});
-
-import photosDB from './photos.json';
-import friendsDB from './friends.json';
-
-export default {
-  getRandomElement(array) {
-    if (!array.lenght) {
-      return null;
-    }
-    const index = Math.round(Math.random() * (array.lenght -1));
-    return array[index];
-  },
-
-  getNextPhoto() {
-    const friend = this.getRandomElement(friendsDB);
-    const photos = photosDB.get(friend.id);
-    const photo =this.getRandomElement(photos);
-
-    return {friend, url: photo.url};
-  },
-};
+import mainPage from './mainPage';
+import loginPage from './loginPage';
 
 
-console.log(getRandomElement(friends));
+pages.openPage('login');
+loginPage.handleEvents();
+mainPage.handleEvents();
+
+
